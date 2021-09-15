a0eb9bb6ac7b03d6b50808848ff761d608f7fe8b
MaartensInformatica <90286141+MaartensInformatica@users.noreply.github.com>
2021-09-15 14:09:57 +0200

    Update wifi troubleshoot.py

--- wifi troubleshoot.py
+++ wifi troubleshoot.py
@@ -1,7 +1,10 @@
+#In beginsel werkt de wifi niet dus starten we met de eerste stap
 print('reboot the computer and try to connect')
 
+#Vraag of de eerste stap geholpen heeft
 answer = input('did that fix the problem?: ')
 
+#Iedere keer dat er nee geantwoord wordt, printen we een nieuwe vraag. Daarom if, if, if etc..
 if answer == 'no':
     print('reboot the router and try to connect')
     answer2 = input('did that fix the problem?: ')
@@ -15,5 +18,6 @@ if answer == 'no':
                 print('get a new router')
 if answer or answer2 or answer3 or answer4 == 'yes':
     print('doei')
-    
+#De code waarbij ja als antwoord wordt gegeven moet op bij elke vraag van toepassing zijn. 
+#Daarom staat deze op hetzelfde niveau als de eerste vraag maar heeft het wel alle anwtwoordmomenten als variabelen  
     
