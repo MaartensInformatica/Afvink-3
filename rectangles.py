15ff7e7882c508c55b655a0c9a51a861099fbf08
MaartensInformatica <90286141+MaartensInformatica@users.noreply.github.com>
2021-09-15 13:57:14 +0200

    Update rectangles.py

--- rectangles.py
+++ rectangles.py
@@ -1,11 +1,14 @@
+#maak 4 variabelen aan voor de 4 afmetingen en laat de gebruiker deze invoeren
 lengte1 = input('lengte eerste rechthoek: ')
 breedte1 = input('breedte eerste rechthoek: ')
 lengte2 = input('lengte tweede rechthoek: ')
 breedte2 = input('breedte tweede rechthoek: ')
 
+#bereken de oppervlaktes
 oppervlakte1 = float(lengte1)*float(breedte1)
 oppervlakte2 = float(lengte2)*float(breedte2)
 
+#gebruik if als rechthoek 1 groter is, elif als dat niet waar is en rechthoek 2 groter is en else als beide onwaar zijn.
 if oppervlakte1 > oppervlakte2: print('rechthoek 1 is groter')
 elif oppervlakte1 < oppervlakte2: print('rechthoek 2 is groter')
 else: print('rechthoeken zijn even groot')
