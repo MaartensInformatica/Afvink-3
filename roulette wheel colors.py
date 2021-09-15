0a5f1caced89cd2d549a49a4bec85bc3c23b6923
MaartensInformatica <90286141+MaartensInformatica@users.noreply.github.com>
2021-09-15 14:03:44 +0200

    Update roulette wheel colors.py

--- roulette wheel colors.py
+++ roulette wheel colors.py
@@ -1,5 +1,8 @@
+#Maak een variabele aan en vraag om een float
 pocket = float(input('insert pocket number: '))
 
+#We zoeken naar een getal tussen twee marges en of het even of oneven is. Daarom gebruiken we '> and < and %2'. 
+#Als de eerste niet waar is gaan we naar de volgende. Dus if, elif, elif etc... totdat alle ifs onwaar zijn. In dat geval is de invoer buiten de marge. Dan gebruiken we else.
 if pocket > 0 and pocket < 11 and pocket %2 == 1:
     print('red')
 elif pocket > 0 and pocket < 11 and pocket %2 == 0:
